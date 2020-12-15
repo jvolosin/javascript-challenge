09cdad47d9d8710a0afc5ecd09ba87910bb210d4
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2020-12-15 11:10:15 -0500

    using day 3 ativity 9 as template for filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -35,3 +35,38 @@ tableData.forEach((ufo) => {
 
 //Use a date form in your HTML document and write JavaScript code 
 //that will listen for events and search through the `date/time` column to find rows that match user input.
+
+
+// Assign the data from `data.js` to a descriptive variable
+var people = data;
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#patient-form-input");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(people);
+
+  var filteredData = people.filter(person => person.bloodType === inputValue);
+
+  console.log(filteredData);
+
+};
