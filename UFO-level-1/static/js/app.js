1fa2cb2aac948d16a402d2e359538fe29d9174d3
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2020-12-15 12:08:28 -0500

    working on filter button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -36,12 +36,10 @@ tableData.forEach((ufo) => {
 //Use a date form in your HTML document and write JavaScript code 
 //that will listen for events and search through the `date/time` column to find rows that match user input.
 
-
-// Assign the data from `data.js` to a descriptive variable
-var people = data;
+var tableData = data;
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
 var form = d3.select("#form");
@@ -57,15 +55,15 @@ function runEnter() {
   d3.event.preventDefault();
   
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#patient-form-input");
+  var inputElement = d3.select("datetime");
 
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  console.log(people);
+  console.log(tableData);
 
-  var filteredData = people.filter(person => person.bloodType === inputValue);
+  var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
 
   console.log(filteredData);
 
